b3712e9e34dddefe0c1d62f1fddfe4fea4c2a4fb
eduard02022 <perezgarciaeduardo150@gmail.com>
2022-12-05 17:40:20 +0100

    probando equals con strings

--- Main.java
+++ Main.java
@@ -1,49 +1,19 @@
 
-import java.util.Arrays
-public class Main {
+public class CompareWithEquals {
 
     public static void main(String[] args) {
-        @@ -22,8 +24,39 @@ public static void main(String[] args) {
-            modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
-            System.out.println("Valor del objeto fuera del método: " + objeto2);
+        String s1 = "hola";
+        String s2 = s1;
+        String s3 = "hola";
+        String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
 
+        System.out.println(s1 == s2); // true
+        System.out.println(s1 == s3); // true
+        System.out.println(s1 == s4); // false: referencias distintas
+        System.out.println(s1.equals(s2)); // true
+        System.out.println(s1.equals(s3));  // true
+        System.out.println(s1.equals(s4)); // true: referencias distintas a objetos iguales
 
-
-            System.out.println("\n--- Pasando arrays");
-
-            int[] array1 = new int[5];
-            printArray(array1);
-            array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
-
-            int[] array2 = array1;  // dos variables apuntando al mismo array
-            int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
-
-            IncrementaArray(array1);
-            IncrementaArray(array2);
-            IncrementaArray(array3);
-
-            printArray(array1);
-            printArray(array2);
-            printArray(array3);
-        }
-
-        private static void IncrementaArray(int[] array) {
-            for (int i = 0; i < array.length; i++) {
-                array[i] = array[i] + 1;
-            }
-        }
-
-
-        private static void printArray(int[] array) {
-            Arrays.stream(array).forEach(System.out::print);
-            System.out.println();
-        }
-
-
-        private static void modificaVarInt(int a){
-            a++; // Incrementamos en 1
-            System.out.println("Valor del int dentro del método:   " + a);
-
-        }
     }
+
 }
